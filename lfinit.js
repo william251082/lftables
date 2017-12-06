72fb42bd5b58dc5ff6c1b811461f02730fcfd555
williamdelrosario <williamdelrosario@yahoo.com>
2017-12-06 13:21:56 +0100

    Second init

--- lfinit.js
+++ lfinit.js
@@ -1,3 +1,60 @@
 // Define rituals.db namespace
 
-var Rituals = { db: {} };
+//var Rituals = { db: {} };
+
+/**
+ * A singleton service used by the rest of the application to make calls to the
+ * Lovefield API.
+ * @constructor
+ */
+
+var LovefieldService = function() {
+  // Following member variables are initialized within getDbConnection().
+  this.db_ = null;
+  this.su_ = null;
+  this.ql_ = null;
+  this.im_ = null;
+};
+
+/**
+ * Initializes member variables that can't be initialized before getting a
+ * connection to the database.
+ * @private
+ */
+LovefieldService.prototype.onConnected_ = function() {
+  this.su_ = this.db_.getSchema().table('Surveys');
+  this.ql_ = this.db_.getSchema().table('Questionlists');
+  this.im_ = this.db_.getSchema().table('Images');
+  alert('DB connection established.');
+};
+
+
+/**
+ * Instantiates the DB connection (re-entrant).
+ * @return {!IThenable<!lf.Database>}
+ */
+LovefieldService.prototype.getDbConnection = function() {
+  if (this.db_ != null) {
+    return this.db_;
+  }
+
+  var schemaBuilder = this.buildSchema_();
+
+  // This is necessary for the app to run with no errors on the 1st step.
+  if (schemaBuilder == null) {
+    return Promise.resolve(null);
+  }
+
+  var connectOptions = {storeType: lf.schema.DataStoreType.INDEXED_DB};
+  return this.buildSchema_().connect(connectOptions).then(
+      function(db) {
+        this.db_ = db;
+        this.onConnected_();
+        return db;
+      }.bind(this));
+};
+
+
+
+
+
--- lftables.js
+++ lftables.js
@@ -1,29 +1,29 @@
-/* define surveys, images, questionlists table */
-Rituals.db.getSchemaBuilder = function(){
-var schemaBuilder = lf.schema.create('rituals', 1);
-schemaBuilder.createTable('Surveys').
+/**
+ * Builds the database schema.
+ * @return {!lf.schema.Builder}
+ * @private
+ */
+LovefieldService.prototype.buildSchema_ = function() {
+  // Step1: Create three tables, Surveys, Questionlists' and Images.
+   var schemaBuilder = lf.schema.create('rituals', 1);
+
+    /* define surveys table */
+    schemaBuilder.createTable('Surveys').
     addColumn('questionlist_id', lf.Type.INTEGER).
     addColumn('name', lf.Type.STRING).
     addColumn('type', lf.Type.STRING).
     addColumn('status', lf.Type.STRING).
     addColumn('location_id', lf.Type.STRING).
-    addColumn('edited', lf.Type.STRING).
+    addColumn('edited', lf.Type.DATE_TIME).
     addColumn('progress', lf.Type.STRING).    // % completed
     addColumn('questions', lf.Type.OBJECT).
     addColumn('confirmation', lf.Type.OBJECT).
     addColumn('email_cc', lf.Type.OBJECT).  // ex. signatures
-   	addColumn('done', lf.Type.BOOLEAN).		// ex. signatures
+    addColumn('done', lf.Type.BOOLEAN).     // ex. signatures
     addPrimaryKey(['id']);
     // addIndex('idxDeadline', ['deadline'], false, lf.Order.DESC); 
-
-    schemaBuilder.createTable('Images').
-    addColumn('survey_id', lf.Type.STRING).
-    addColumn('question_id', lf.Type.INTEGER).
-    addColumn('created', lf.Type.STRING). // DATE uses timestamp
-    addColumn('base64', lf.Type.STRING).
-    addColumn('thumb', lf.Type.STRING).
-    addIndex('idxDeadline', ['survey_id'], false, lf.Order.DESC);
  
+
     /* define questionlists table */
     schemaBuilder.createTable('Questionlists').
     addColumn('questionlist_id', lf.Type.INTEGER).
@@ -32,9 +32,19 @@ schemaBuilder.createTable('Surveys').
     addColumn('language', lf.Type.STRING).
     addColumn('roles', lf.Type.STRING).
     addColumn('type', lf.Type.STRING).
-    addColumn('created', lf.Type.STRING).    // % completed
+    addColumn('created', lf.Type.DATE_TIME).    // % completed
     addColumn('questions', lf.Type.OBJECT).
     addIndex('idxDeadline', ['questionlist_id'], false, lf.Order.DESC); // set index
+
+
+    /* define images table */
+    schemaBuilder.createTable('Images').
+    addColumn('survey_id', lf.Type.STRING).
+    addColumn('question_id', lf.Type.INTEGER).
+    addColumn('created', lf.Type.DATE_TIME). // DATE uses timestamp
+    addColumn('base64', lf.Type.STRING).
+    addColumn('thumb', lf.Type.STRING).
+    addIndex('idxDeadline', ['survey_id'], false, lf.Order.DESC);
 
 
     return schemaBuilder;
@@ -42,4 +52,33 @@ schemaBuilder.createTable('Surveys').
 
 
 
+var SurveysRaw;
+
+var QuestionlistsRaw;
+
+var ImagesRaw;
+
+
+/**
+ * Inserts data in the three tables, Surveys, Questionlists and Images.
+ * @param {!Array<!SurveysRaw>} SurveysRaw
+ * @param {!Array<!QuestionlistsRaw>} QuestionlistsRaw
+ * @param {!Array<!QuestionlistsRaw>} ImagesRaw
+ * @return {!IThenable} A promise that is resolved after both tables have been
+ *     populated.
+ */
+LovefieldService.prototype.insertData = function(
+    surveysRaw, questionlistsRaw, imagesRaw) {
+   // Generating Lovefield rows from the raw rows.
+  var surveysRaw = surveysRaw.map(
+      function(obj) { return this.si_.createRow(obj); }, this);
+  var questionlistsRaw = questionlistsRaw.map(
+      function(obj) { return this.si_.createRow(obj); }, this);
+  var imagesRaw = imagesRaw.map(
+      function(obj) { return this.si_.createRow(obj); }, this);
+
+  
+  // Step2: Insert the raw data to the database.
+  return Promise.resolve();
+};
 
